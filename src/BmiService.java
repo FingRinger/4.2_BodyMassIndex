b116d2198dc1502f98708d519d6d9a69115542aa
FingRinger <ard21study@gmail.com>
2021-06-10 18:17:06 +0300

    Removing unnecessary values ​​from a method in the BonusService class

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,6 @@
 public class BmiService {
     public double calculate (double weight, double height){
-        weight = 56;
-        height = 1.68;
+
         double bodyMassIndex = weight / (height * height);
 
         return bodyMassIndex;
